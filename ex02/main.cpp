339f159c808279df9cd55b73ee8a817cc7d3f4d2
Nartex <garadraw@gmail.com>
2022-11-17 21:52:39 +0100

    Added ex02 for Testing on MAC

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,11 +6,73 @@
 /*   By: vsimeono <vsimeono@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 20:19:47 by vsimeono          #+#    #+#             */
-/*   Updated: 2022/11/17 20:35:39 by vsimeono         ###   ########.fr       */
+/*   Updated: 2022/11/17 21:51:57 by vsimeono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
+
+
+#include "Base.hpp"
+
+Base *generate(void)
+{
+	srand(time(0));
+	int numb = rand() % 10 + 1;
+	if (numb <= 3)
+		return (new A);
+	else if (numb <= 6)
+		return (new B);
+	return (new C);
+}
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "Class type A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "Class type B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "Class type C" << std::endl;
+	else
+		std::cout << "Class type Unknown" << std::endl;
+}
+
+void identify(Base &p)
+{
+	Base temp;
+
+	try
+	{
+		temp = dynamic_cast<A&>(p);
+		std::cout << "Class type A" << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	try
+	{
+		temp = dynamic_cast<B&>(p);
+		std::cout << "Class type B" << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	try
+	{
+		temp = dynamic_cast<C&>(p);
+		std::cout << "Class type C" << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+}
+
 
 int main()
 {
